844b1563c6370eeba90a3260035f0141f85def8f
Mikkel Arentoft <mar@rehfeld.dk>
2016-10-10 16:09:17 +0200

    Writing context

--- hooks/before_plugin_install.js
+++ hooks/before_plugin_install.js
@@ -3,9 +3,7 @@ module.exports = function(ctx) {
     /*if (ctx.opts.platforms.indexOf('android') < 0) {
         return;
     }*/
-	console.log(ctx != null ? "CTX" : "-CTX")
-	console.log(ctx.opts != null ? "CTX.OPTS" : "-CTX.OPTS")
-	console.log(ctx.opts.platforms != null ? ctx.opts.platforms : "-CTX.OPTS.PLATFORMS")
+	console.log(ctx != null ? JSON.stringify(ctx) : "-CTX")
 	console.log("************************************");
 	console.log("* Hello from Before Plugin Install *");
 	console.log("************************************");
