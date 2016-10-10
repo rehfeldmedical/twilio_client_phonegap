7d4089ff1b4269855b188e6fdfefa1560e87de2a
Mikkel Arentoft <mar@rehfeld.dk>
2016-10-10 16:16:16 +0200

    Logging the dir

--- hooks/before_plugin_install.js
+++ hooks/before_plugin_install.js
@@ -1,9 +1,11 @@
 module.exports = function(ctx) {
     // make sure android platform is part of build
-    /*if (ctx.opts.platforms.indexOf('android') < 0) {
+    if (ctx.opts.cordova.platforms.indexOf('ios') < 0) {
         return;
-    }*/
-	console.log(ctx != null ? JSON.stringify(ctx) : "-CTX")
+    }
+	
+	console.log(ctx.opts.plugin.pluginInfo.dir)
+	
 	console.log("************************************");
 	console.log("* Hello from Before Plugin Install *");
 	console.log("************************************");
