435f8bc096be6dd58cc63b455620419add20e29e
Mikkel Arentoft <mar@rehfeld.dk>
2016-10-10 15:59:10 +0200

    Removing the test just to see it works

--- hooks/before_plugin_install.js
+++ hooks/before_plugin_install.js
@@ -1,8 +1,8 @@
 module.exports = function(ctx) {
     // make sure android platform is part of build
-    if (ctx.opts.platforms.indexOf('android') < 0) {
+    /*if (ctx.opts.platforms.indexOf('android') < 0) {
         return;
-    }
+    }*/
 	console.log("************************************");
 	console.log("* Hello from Before Plugin Install *");
 	console.log("************************************");
