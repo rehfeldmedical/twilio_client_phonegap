ffe634a4f61763c502dcab9bee1957a1f30d50d5
Mikkel Arentoft <mar@rehfeld.dk>
2016-10-10 16:25:29 +0200

    First cut on unzip

--- hooks/before_plugin_install.js
+++ hooks/before_plugin_install.js
@@ -4,7 +4,14 @@ module.exports = function(ctx) {
         return;
     }
 	
-	console.log(ctx.opts.plugin.pluginInfo.dir)
+	var fs = ctx.requireCordovaModule('fs');
+	var path = ctx.requireCordovaModule('path');
+	var zlib = ctx.requireCordovaModule('zlib');
+	
+	//var untar = require('untar')
+	var srcDir = path.join(ctx.opts.plugin.pluginInfo.dir, 'src/ios');
+	fs.createReadStream(path.join(srcDir, 'twilio-client-ios-1.2.11.tar.bz2').pipe(unzip.Extract({ path: srcDir }));
+	//console.log(ctx.opts.plugin.pluginInfo.dir)
 	
 	console.log("************************************");
 	console.log("* Hello from Before Plugin Install *");
