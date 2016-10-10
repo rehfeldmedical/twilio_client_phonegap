3ee00cc4468dcbdb9496d32b39a9c94501ed2e83
Mikkel Arentoft <mar@rehfeld.dk>
2016-10-10 15:57:05 +0200

    Rewrote the before plugin install hook to Node

--- hooks/before_plugin_install.js
+++ hooks/before_plugin_install.js
@@ -1,4 +1,9 @@
-#!/usr/bin/env node
-console.log("************************************");
-console.log("* Hello from Before Plugin Install *");
-console.log("************************************");
+module.exports = function(ctx) {
+    // make sure android platform is part of build
+    if (ctx.opts.platforms.indexOf('android') < 0) {
+        return;
+    }
+	console.log("************************************");
+	console.log("* Hello from Before Plugin Install *");
+	console.log("************************************");
+}
