bd7ef06fbb17ca53427107704baeb7c82142d818
Mikkel Arentoft <mar@rehfeld.dk>
2016-10-10 16:06:16 +0200

    Checking the platforms of the context

--- hooks/before_plugin_install.js
+++ hooks/before_plugin_install.js
@@ -3,6 +3,9 @@ module.exports = function(ctx) {
     /*if (ctx.opts.platforms.indexOf('android') < 0) {
         return;
     }*/
+	console.log(ctx != null ? "CTX" : "-CTX")
+	console.log(ctx.opts != null ? "CTX.OPTS" : "-CTX.OPTS")
+	console.log(ctx.opts.platforms != null ? ctx.opts.platforms : "-CTX.OPTS.PLATFORMS")
 	console.log("************************************");
 	console.log("* Hello from Before Plugin Install *");
 	console.log("************************************");
