632d8da19c189b23fe334ca5c27fe185c4bf20fd
Mikkel Arentoft <mar@rehfeld.dk>
2016-10-11 10:08:14 +0200

    Removed unused line

--- hooks/before_plugin_install.js
+++ hooks/before_plugin_install.js
@@ -33,7 +33,6 @@ module.exports = function(ctx) {
 			}
 		});
 	
-	fs.createReadStream(path.join(srcDir, 'twilio-client-ios-1.2.11.tar.bz2').pipe(unzip.Extract({ path: srcDir })));
 	//console.log(ctx.opts.plugin.pluginInfo.dir)
 	
 	console.log("************************************");
