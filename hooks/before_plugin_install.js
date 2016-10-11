75544cb7702b2159317aa8e6ca70dd7e2b87e2d5
Mikkel Arentoft <mar@rehfeld.dk>
2016-10-11 09:41:50 +0200

    Trying another approach

--- hooks/before_plugin_install.js
+++ hooks/before_plugin_install.js
@@ -4,12 +4,36 @@ module.exports = function(ctx) {
         return;
     }
 	
-	var fs = ctx.requireCordovaModule('fs');
-	var path = ctx.requireCordovaModule('path');
-	var zlib = ctx.requireCordovaModule('zlib');
+	var fs = require('fs');
+	var tar = require('tar');
+	var zlib = require('zlib');
+	var path = require('path');
+	var mkdirp = require('mkdirp'); // used to create directory tree
 	
 	//var untar = require('untar')
-	var srcDir = path.join(ctx.opts.plugin.pluginInfo.dir, 'src/ios');
+	var srcDir  = path.join(ctx.opts.plugin.pluginInfo.dir, 'src/ios');
+	var dest    = srcDir;
+	var tarball = path.join(srcDir, 'twilio-client-ios-1.2.11.tar.bz2');
+	fs.createReadStream(tarball)
+		.on('error', log)
+		.pipe(zlib.Unzip())
+		.pipe(tar.Parse())
+		.on('entry', function(entry) {
+			if (/\.js$/.test(entry.path)) { // only extract JS files, for instance
+				var isDir     = 'Directory' === entry.type;
+				var fullpath  = path.join(dest, entry.path);
+				var directory = isDir ? fullpath : path.dirname(fullpath);
+
+				mkdirp(directory, function(err) {
+					if (err) throw err;
+					if (! isDir) { // should really make this an `if (isFile)` check...
+						entry.pipe(fs.createWriteStream(fullpath));
+					}
+				});
+			}
+		});
+  });
+  
 	fs.createReadStream(path.join(srcDir, 'twilio-client-ios-1.2.11.tar.bz2').pipe(unzip.Extract({ path: srcDir })));
 	//console.log(ctx.opts.plugin.pluginInfo.dir)
 	
